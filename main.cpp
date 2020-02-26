8a0e204db2a603c2a987abdf9657b1d03e7a02f3
Kevin Chan <kevinchan128@msn.com>
2020-02-26 00:02:45 -0800

    Commit 4
    Working on Part A.
    Currently only have file stream to open the two files we're trying to fully read.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <fstream>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    //Part A: Our program must produce identical output: ASMT02_PA_Run1.txt
+    //and ASMT02_PA_Run2.txt
+    //do a file IO stream to read both of the entire files
+
+    cout << "Part A: Reading 2 Files" << endl;
+
+    ofstream fileIO;
+
+    fileIO.open("/Assignment-02-Code/ASMT02_PA_Run1.txt");
+    fileIO.open("/Assignment-02-Code/ASMT02_PA_Run2.txt");
+
+    
+
     return 0;
 }

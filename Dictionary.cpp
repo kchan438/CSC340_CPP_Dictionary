d5d1200df9a2921dd657e34aa18ae23bf5964c20
Kevin Chan <kevinchan128@msn.com>
2020-03-03 00:04:20 -0800

    Commit 10
    Dictionary.cpp was placed in the wrong folder.
    Also these .cpp and .h files were created so that we can use them for implementing hashmaps

--- Dictionary.cpp
+++ Dictionary.cpp
@@ -2,6 +2,6 @@
 // Created by Kevin Chan on 3/2/20.
 //
 #include "Dictionary.h"
-Dictionary::Dictionary() { }    //constructor
+Dictionary::Dictionary() = default;    //constructor
 
 //this is where we will create dictionary hashmaps and store data here

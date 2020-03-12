ba534f23c428f2965b34098d0a99c6a2688773b9
Kevin Chan <kevinchan128@msn.com>
2020-03-12 13:49:45 -0700

    Fixed CMake Debug File
    .gitignore ignored all cmake files

---
+++ cmake-build-debug/CMakeFiles/CSC340_CPP_Dictionary.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/kevinchan/Documents/GitHub/CSC340_CPP_Dictionary/Dictionary.cpp" "/Users/kevinchan/Documents/GitHub/CSC340_CPP_Dictionary/cmake-build-debug/CMakeFiles/CSC340_CPP_Dictionary.dir/Dictionary.cpp.o"
+  "/Users/kevinchan/Documents/GitHub/CSC340_CPP_Dictionary/main.cpp" "/Users/kevinchan/Documents/GitHub/CSC340_CPP_Dictionary/cmake-build-debug/CMakeFiles/CSC340_CPP_Dictionary.dir/main.cpp.o"
+  "/Users/kevinchan/Documents/GitHub/CSC340_CPP_Dictionary/readData.cpp" "/Users/kevinchan/Documents/GitHub/CSC340_CPP_Dictionary/cmake-build-debug/CMakeFiles/CSC340_CPP_Dictionary.dir/readData.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/CSC340_CPP_Dictionary.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CSC340_CPP_Dictionary.dir/Dictionary.cpp.o"
+  "CMakeFiles/CSC340_CPP_Dictionary.dir/main.cpp.o"
+  "CMakeFiles/CSC340_CPP_Dictionary.dir/readData.cpp.o"
+  "CSC340_CPP_Dictionary"
+  "CSC340_CPP_Dictionary.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CSC340_CPP_Dictionary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
